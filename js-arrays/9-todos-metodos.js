799ed12050526a866f1154f0f43e461a1b36ebaf
Alfredo Sánchez <alfred3ks@gmail.com>
2022-04-09 04:43:34 +0200

    Ejemplo de algunos metodos vistos.

---
+++ js-arrays/9-todos-metodos.js
@@ -0,0 +1,59 @@
+/*
+Vamos a utilizar todos los metodos vistos:
+*/
+
+const students = [
+    {
+        name: "Jill",
+        lastname: "Doe",
+        age: 23,
+        course: "Marketing",
+    },
+    {
+        name: "John",
+        lastname: "Doe",
+        age: 20,
+        course: "Web Development",
+    },
+    {
+        name: "Jack",
+        lastname: "Doe",
+        age: 22,
+        course: "Accounting",
+    },
+    {
+        name: "Ryan",
+        lastname: "Ray",
+        age: 20,
+        course: "Web Development",
+    },
+    {
+        name: "Jane",
+        lastname: "Doe",
+        age: 21,
+        course: "Financial Management",
+    },
+];
+
+
+// Destructuracion de objetos:
+const result = students.map(({name, lastname, age})=>{
+    return {
+        student: `${name} ${lastname}`,
+        age
+    }
+}).filter((student)=>{
+    if(student.age > 20){
+        return student
+    }
+}).sort((a,b)=>{
+    if(a.age - b.age ){
+        return -1
+    }
+}).reduce((total, student)=> total + student.age, 0
+)
+
+console.log(result);
+
+// Aqui podemos ver aplicados algunos de los metodos que hemos visto.
+// Seguimos.....
